ae3176954e82bd5a81a32634751a7425dc27b9ac
윤상흠 <kidsncar@naver.com>
2025-03-18 13:08:08 +0900

    feat : [SU] 가중합 방식 초기세팅

---
+++ Data/services/score_utils.py
@@ -0,0 +1,34 @@
+# analysis_server/services/score_utils.py
+def calculate_music_score(features: dict) -> dict:
+    """
+    Spotify Audio Features → MUSIC 모델 5차원 점수 변환 함수
+    :param features: valence, acousticness, instrumentalness, etc.
+    :return: MUSIC 5차원 점수 (dict)
+    """
+    mellow = 0.4 * features.get('valence', 0) + \
+             0.3 * features.get('acousticness', 0) + \
+             0.3 * features.get('instrumentalness', 0)
+
+    unpretentious = 0.4 * features.get('acousticness', 0) + \
+                    0.3 * features.get('speechiness', 0) + \
+                    0.3 * features.get('liveness', 0)
+
+    sophisticated = 0.4 * features.get('tempo', 0) + \
+                    0.3 * features.get('instrumentalness', 0) + \
+                    0.3 * features.get('time_signature', 0)
+
+    intense = 0.5 * features.get('energy', 0) + \
+              0.3 * features.get('loudness', 0) + \
+              0.2 * features.get('tempo', 0)
+
+    contemporary = 0.4 * features.get('danceability', 0) + \
+                   0.3 * features.get('speechiness', 0) + \
+                   0.3 * features.get('energy', 0)
+
+    return {
+        "mellow": round(mellow, 4),
+        "unpretentious": round(unpretentious, 4),
+        "sophisticated": round(sophisticated, 4),
+        "intense": round(intense, 4),
+        "contemporary": round(contemporary, 4)
+    }
