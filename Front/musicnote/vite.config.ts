87a46fb2adfa691a4e709c0d834549cfdcc2d8ac
hyjoo1226 <sparrow9576@gmail.com>
2025-03-20 13:39:56 +0900

    init: tailwind 초기세팅

--- Front/musicnote/vite.config.ts
+++ Front/musicnote/vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // @ts-ignore
-import eslint from 'vite-plugin-eslint';
-
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
   plugins: [
     react(),
     eslint({
       fix: true, // 자동으로 ESLint 오류 수정
-    })],
+    }),
+  ],
   esbuild: {
     jsxInject: `import React from 'react'`, // JSX 사용 시 React 자동 import
   },
   resolve: {
     alias: {
-      '@': '/src', // 절대 경로 import 지원
+      "@": "/src", // 절대 경로 import 지원
     },
+  },
+  css: {
+    postcss: "./postcss.config.cjs",
   },
 });
