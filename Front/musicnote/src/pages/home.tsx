f0742b9619d0df3999eb0784e126cbb19c5a50e8
주현호 <sparrow9576@naver.com>
2025-03-24 13:55:28 +0900

    [FE] Feature - Navbar 구현

--- Front/musicnote/src/pages/home.tsx
+++ Front/musicnote/src/pages/home.tsx
@@ -1,13 +1,12 @@
 import UserTemperGraph from "../components/UserTemperGraph";
 import logo from "@/assets/logo/logo.png";
 import shortLogo from "@/assets/logo/short-logo.png";
-import recentPlayedList from "@/assets/data/recent-played-list.json";
+import recentPlayedList from "../assets/data/recent-played-list.json";
 // import { useGetData } from "../hooks/useApi";
 
 export default function Home() {
-  const wordOfToday = "오늘은 외향성이 높으시네요. 사람들과 어울리면서 많은 에너지를 얻기를 바라요!";
-
-  // const { data: recentPlayedList } = useGetData('recentPlayedList', '/api/v1/music/recent-played-list');
+  const wordOfToday =
+    "오늘은 외향성이 높으시네요. 사람들과 어울리면서 많은 에너지를 얻기를 바라요!";
 
   return (
     <div className="flex flex-col items-center h-screen bg-level1 p-6 w-full">
@@ -19,31 +18,61 @@ export default function Home() {
       {/* 메인 페이지 */}
       <div className="flex flex-col gap-y-4 justify-between h-full">
         <div className="flex flex-col items-center justify-center w-full bg-level2 rounded-lg p-4 gap-y-2">
-          <span className="text-white text-xl font-medium self-start">오늘의 한 마디</span>
+          <span className="text-white text-xl font-medium self-start">
+            오늘의 한 마디
+          </span>
           <div className="flex flex-row items-center justify-start gap-x-2">
             <div className="flex-shrink-0 w-[30px] h-[30px]">
-              <svg width="30" height="30" viewBox="0 0 26 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M15.2181 16.9066V21.3835C15.2181 22.09 14.6503 22.6626 13.9499 22.6626H11.4135C10.7132 22.6626 10.1454 22.09 10.1454 21.3835V16.9066M19.0226 11.1505C19.0226 14.6827 16.1837 17.5462 12.6817 17.5462C9.17974 17.5462 6.34082 14.6827 6.34082 11.1505C6.34082 7.61835 9.17974 4.75494 12.6817 4.75494C16.1837 4.75494 19.0226 7.61835 19.0226 11.1505Z" stroke="#FE365E" strokeWidth="2"/>
+              <svg
+                width="30"
+                height="30"
+                viewBox="0 0 26 27"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15.2181 16.9066V21.3835C15.2181 22.09 14.6503 22.6626 13.9499 22.6626H11.4135C10.7132 22.6626 10.1454 22.09 10.1454 21.3835V16.9066M19.0226 11.1505C19.0226 14.6827 16.1837 17.5462 12.6817 17.5462C9.17974 17.5462 6.34082 14.6827 6.34082 11.1505C6.34082 7.61835 9.17974 4.75494 12.6817 4.75494C16.1837 4.75494 19.0226 7.61835 19.0226 11.1505Z"
+                  stroke="#FE365E"
+                  strokeWidth="2"
+                />
               </svg>
             </div>
-            <span className="flex-1 text-light-gray text-[16px] font-medium whitespace-normal break-keep">{wordOfToday}</span>
+            <span className="flex-1 text-light-gray text-[16px] font-medium whitespace-normal break-keep">
+              {wordOfToday}
+            </span>
           </div>
         </div>
-        <UserTemperGraph scores = {[75, 59, 85, 39, 51]}/>
+        <UserTemperGraph scores={[75, 59, 85, 39, 51]} />
         <div className="flex flex-col items-center justify-center w-full bg-level2 rounded-lg p-4 gap-y-2">
-          <span className="text-white text-xl font-medium self-start">최근에 들은 음악</span>
+          <span className="text-white text-xl font-medium self-start">
+            최근에 들은 음악
+          </span>
           <div className="flex flex-col items-start justify-start w-full gap-y-3">
-            {Array.isArray(recentPlayedList.items) && recentPlayedList.items.slice(0, 3).map((item: Array<string>, index: number) => (
-              <div key={index} className="recent-played-item flex flex-row items-center justify-start gap-x-2 w-full">
-                <div className="flex flex-row items-center justify-start gap-x-4 px-4 py-1 rounded-lg">
-                  <img src={item.track?.album.images[0].url} alt={item.track?.name} className="w-12 h-12 rounded-lg" />
-                  <div className="flex flex-col">
-                    <span className="text-light-gray text-sm font-light">{item.track?.artists[0].name || "Unknown Artist"}</span>
-                    <span className="text-white text-sm font-medium">{item.track?.name || "Unknown Track"}</span>
+            {Array.isArray(recentPlayedList.items) &&
+              recentPlayedList.items
+                .slice(0, 3)
+                .map((item: any, index: number) => (
+                  <div
+                    key={index}
+                    className="recent-played-item flex flex-row items-center justify-start gap-x-2 w-full"
+                  >
+                    <div className="flex flex-row items-center justify-start gap-x-4 px-4 py-1 rounded-lg">
+                      <img
+                        src={item.track?.album.images[0].url}
+                        alt={item.track?.name}
+                        className="w-12 h-12 rounded-lg"
+                      />
+                      <div className="flex flex-col">
+                        <span className="text-light-gray text-sm font-light">
+                          {item.track?.artists[0].name || "Unknown Artist"}
+                        </span>
+                        <span className="text-white text-sm font-medium">
+                          {item.track?.name || "Unknown Track"}
+                        </span>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            ))}
+                ))}
           </div>
         </div>
       </div>
--- Front/musicnote/src/routes/AppRoutes.tsx
+++ Front/musicnote/src/routes/AppRoutes.tsx
@@ -1,10 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home.tsx";
+import Analysis from "../pages/Analysis.tsx";
+import Discover from "../pages/Discover.tsx";
+import Recommendations from "../pages/Recommendations.tsx";
 
 export default function AppRoutes(): React.ReactElement {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/analysis" element={<Analysis />} />
+      <Route path="/discover" element={<Discover />} />
+      <Route path="/recommendations" element={<Recommendations />} />
     </Routes>
-  )
+  );
 }
