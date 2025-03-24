1f18248c2c3fbcb83a3fb6ad1175360078cdd86e
민경현 <skyblue848@naver.com>
2025-03-24 12:47:27 +0900

    [FE] Feature - react query 기본 설정

---
+++ Front/musicnote/src/hooks/useApi.js
@@ -0,0 +1,46 @@
+import { useQuery, useMutation } from '@tanstack/react-query';
+import apiClient from '../api/apiClient';
+
+// GET 요청을 위한 커스텀 훅
+export const useGetData = (key, url, options = {}) => {
+  return useQuery({
+    queryKey: [key],
+    queryFn: () => apiClient.get(url).then(res => res.data),
+    ...options
+  });
+};
+
+// POST 요청을 위한 커스텀 훅
+export const usePostData = (url) => {
+  return useMutation({
+    mutationFn: (data) => apiClient.post(url, data).then(res => res.data)
+  });
+};
+
+
+// 예시
+// 컴포넌트에서 사용
+// import { useGetData, usePostData } from '../hooks/useApi';
+
+// function UserProfile() {
+//   const { data, isLoading, error } = useGetData('user', '/api/user/profile');
+  
+//   if (isLoading) return <div>로딩 중...</div>;
+//   if (error) return <div>에러 발생: {error.message}</div>;
+  
+//   return <div>{data.name}님, 환영합니다!</div>;
+// }
+
+// function CreatePost() {
+//   const postMutation = usePostData('/api/posts');
+  
+//   const handleSubmit = (data) => {
+//     postMutation.mutate(data, {
+//       onSuccess: (response) => {
+//         console.log('성공:', response);
+//       }
+//     });
+//   };
+  
+//   return (/* 폼 컴포넌트 */);
+// }
