72311c8a55fd79f4a242123c2511ec9bd8479152
남기운 <madeinburundi@gmail.com>
2025-03-31 10:25:07 +0900

    fix : [TEST] 전처리파일 수정

--- Data/test/pick_user/preprocess.py
+++ Data/test/pick_user/preprocess.py
@@ -17,6 +17,7 @@ movies = pd.read_csv(movie_file)
 # 전처리
 df.columns = df.columns.str.strip()
 movies.columns = movies.columns.str.strip()
+movies['genres'] = movies['genres'].str.strip()
 
 # enjoy_watching 1, 2 제거
 df = df[~df['enjoy_watching'].isin([1, 2])]
@@ -46,6 +47,9 @@ merged['genres'] = merged['genres'].str.split('|')
 
 # Counter 대신 사용자, 장르 및 개수 열이 있는 새 DataFrame 생성
 genre_df = merged.explode('genres')
+genre_df = genre_df[genre_df['genres'] != 'IMAX']
+print(genre_df)
+print(genre_df['genres'].unique(), len(genre_df['genres'].unique()))
 user_genre_counts = genre_df.groupby(['userid', 'genres']).size().reset_index(name='count')
 
 # 딕셔너리 형식으로 변환
@@ -54,13 +58,7 @@ for userid, group in user_genre_counts.groupby('userid'):
     user_genre_dict[str(userid)] = {row['genres']: int(row['count']) for _, row in group.iterrows()}
 print(user_genre_dict)
 
-# JSON으로 저장
+
+# user_genre_counts 딕셔너리를 JSON 파일로 저장
 with open("user_genre_counts.json", "w") as f:
     json.dump(user_genre_dict, f)
-
-
-
-# print(user_genre_counts)
-# user_genre_counts 딕셔너리를 JSON 파일로 저장
-# with open("user_genre_counts.json", "w") as f:
-#     json.dump(user_genre_counts, f)
