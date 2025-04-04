a2d455477a60c2095434c0de89570a2e3dce1680
hyjoo1226 <sparrow9576@gmail.com>
2025-04-04 10:06:03 +0900

    선택 음악 분석 POST API 임시 구현

--- Front/musicnote/src/pages/discover/ChoiceMusicAnalysis.tsx
+++ Front/musicnote/src/pages/discover/ChoiceMusicAnalysis.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import TopBar from "../../components/layout/TopBar";
 import SearchMusic from "../../features/discover/ChoiceMusicAnalysis/SearchMusic";
 import SelectMusicList from "../../features/discover/ChoiceMusicAnalysis/SelectMusicList";
+import { usePostData } from "../../hooks/useApi";
 
 export default function ChoiceMusicAnalysis() {
   const [selectedTracks, setSelectedTracks] = useState<any[]>([]);
+  const { mutate: postChoiceMusicData } = usePostData("/main/preferences");
 
   // 선택 곡 리스트 추가 핸들러
   const handleTrackSelect = (track: any) => {
@@ -13,9 +15,10 @@ export default function ChoiceMusicAnalysis() {
       const isTrackSelected = prevTracks.some((t) => t.id === track.id);
       if (isTrackSelected) {
         return prevTracks.filter((t) => t.id !== track.id);
-      } else {
+      } else if (prevTracks.length < 20) {
         return [...prevTracks, track];
       }
+      return prevTracks;
     });
   };
   // 선택 곡 리스트 제거 핸들러
@@ -25,14 +28,34 @@ export default function ChoiceMusicAnalysis() {
 
   // 분석하기 버튼 핸들러
   const handleAnalyze = (tracks: any[]) => {
-    const trackIds = tracks.map((track) => track.id);
-    console.log("분석할 트랙 ID:", trackIds);
-    // 여기에 트랙 ID들을 서버로 보내는 로직 추가
+    if (tracks.length === 0) {
+      alert("분석할 곡을 선택해주세요!");
+      return;
+    }
+    // request
+    const musicList = tracks.map((track) => ({
+      spotifyId: track.id,
+      title: track.name,
+      artist: track.artists.map((artist: any) => artist.name).join(", "),
+      imageUrl: track.album?.images?.[0]?.url,
+    }));
+    console.log(musicList);
+
+    // POST API
+    postChoiceMusicData(musicList, {
+      onSuccess: (response) => {
+        console.log("성공적으로 전송되었습니다:", response);
+      },
+      onError: (error) => {
+        console.error("데이터 전송 실패:", error);
+      },
+    });
   };
 
   return (
     <div className="text-white w-full h-full">
       <TopBar title="MyPick 분석" />
+      <div className="text-end pr-10 text-gray-300 my-2">{selectedTracks.length} / 20</div>
       <SearchMusic onTrackSelect={handleTrackSelect} selectedTracks={selectedTracks} />
       <SelectMusicList
         selectedTracks={selectedTracks}
