bcc72efa9a46510e1097dd963d0dca66f1a0090c
윤상흠 <kidsncar@naver.com>
2025-04-04 10:02:04 +0900

    fix: [SC] fix return schema

--- Data/modelschemas/request_response.py
+++ Data/modelschemas/request_response.py
@@ -44,6 +44,20 @@ class Report(BaseModel):
     low_text: str
     summary: str
 
+class Credit(BaseModel):
+    adult: bool
+    gender: int
+    id: int
+    known_for_department: str
+    name: str
+    original_name: str
+    popularity: float
+    profile_path: str
+    cast_id: int
+    character: str
+    credit_id : str
+    order: int
+
 class Movie(BaseModel):
     adult: bool
     backdrop_path: str
@@ -84,11 +98,12 @@ class KeywordList(BaseModel):
 
 class BookItem(BaseModel):
     title: str
+    image: str
     author: str
+    publisher: str
     description: Optional[str]
     isbn: Optional[str]
-    link: str
-    keyword: str
+    pubdate: date
 
 class BookList(BaseModel):
-    results: List[BookItem]
+    books: List[BookItem]
