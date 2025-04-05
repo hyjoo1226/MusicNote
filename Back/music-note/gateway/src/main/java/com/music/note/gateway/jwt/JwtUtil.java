905ef457918d25533a3b0b4771a2d00ffb37d05f
InGyu Moon <ansdlsrb1121@naver.com>
2025-04-05 22:57:58 +0900

    feat: check Authorization token in gateway

---
+++ Back/music-note/gateway/src/main/java/com/music/note/gateway/jwt/JwtUtil.java
@@ -0,0 +1,44 @@
+package com.music.note.gateway.jwt;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JwtUtil {
+
+	//TODO: yml 파일에 secretKey 를 넣고 가져오도록 수정
+
+	private String secretKey = "music-note-jwt-key";
+
+	public void isTokenValid(String token) {
+		try {
+			Jwts.parser()
+				.setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
+				.parseClaimsJws(token);
+
+			log.info("유효한 JWT 토큰입니다.");
+		} catch (Exception e) {
+			log.warn("JWT 검증 실패: {}", e.getMessage());
+			throw new RuntimeException("유효하지 않은 JWT 토큰입니다.");
+		}
+	}
+
+	// ✅ Claims 전체 추출
+	public Claims extractClaims(String token) {
+		return Jwts.parser()
+			.setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
+			.parseClaimsJws(token)
+			.getBody();
+	}
+
+	// ✅ 사용자 ID 추출
+	public String getUserId(String token) {
+		return extractClaims(token).get("id", String.class);
+	}
+}
