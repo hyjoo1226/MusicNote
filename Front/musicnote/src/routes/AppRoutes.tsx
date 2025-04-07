6b3b28d00bed8a6d9ad701a63da3f100544ab1c4
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-07 13:42:13 +0900

    feat: private Route 추가

--- Front/musicnote/src/routes/AppRoutes.tsx
+++ Front/musicnote/src/routes/AppRoutes.tsx
@@ -15,27 +15,35 @@ import RecommendationMovie from "../pages/recommend/RecommendationMovie";
 import RecommendationMusic from "../pages/recommend/RecommendationMusic";
 import RecommendationBook from "../pages/recommend/RecommendationBook";
 import MyRecommendationDetail from "../pages/recommend/MyRecommendationDetail";
+import PrivateRoute from "../components/PrivateRoute";
 
 export default function AppRoutes(): React.ReactElement {
   return (
     <Routes>
+      {/* 인증이 필요없는 라우트 */}
       <Route path="/" element={<Login />} />
       <Route path="/callback" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/musiclist/:title/" element={<MusicList />} />
-      <Route path="/analysis" element={<Analysis />} />
-      <Route path="/analysis/report" element={<Report />} />
-      <Route path="/discover" element={<Discover />} />
-      <Route path="/discover/choice-music-analysis" element={<ChoiceMusicAnalysis />} />
-      <Route path="/discover/line-chart" element={<LineChart />} />
-      <Route path="/recommendations" element={<Recommendations />} />
-      <Route path="/recommendations/detail/movie" element={<RecommendationMovie />} />
-      <Route path="/recommendations/detail/music" element={<RecommendationMusic />} />
-      <Route path="/recommendations/detail/book" element={<RecommendationBook />} />
-      <Route path="/recommendations/my" element={<MyRecommendation />} />
-      <Route path="/recommendations/my/movie" element={<MyRecommendationDetail />} />
-      <Route path="/recommendations/my/music" element={<MyRecommendationDetail />} />
-      <Route path="/notification" element={<Notification />} />
+
+      {/* 인증이 필요한 라우트 */}
+      <Route element={<PrivateRoute />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/musiclist/:title/" element={<MusicList />} />
+        <Route path="/analysis" element={<Analysis />} />
+        <Route path="/analysis/report" element={<Report />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/discover/choice-music-analysis" element={<ChoiceMusicAnalysis />} />
+        <Route path="/discover/line-chart" element={<LineChart />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/recommendations/detail/movie" element={<RecommendationMovie />} />
+        <Route path="/recommendations/detail/music" element={<RecommendationMusic />} />
+        <Route path="/recommendations/detail/book" element={<RecommendationBook />} />
+        <Route path="/recommendations/my" element={<MyRecommendation />} />
+        <Route path="/recommendations/my/movie" element={<MyRecommendationDetail />} />
+        <Route path="/recommendations/my/music" element={<MyRecommendationDetail />} />
+        <Route path="/notification" element={<Notification />} />
+      </Route>
+
+      {/* 404 페이지 */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
