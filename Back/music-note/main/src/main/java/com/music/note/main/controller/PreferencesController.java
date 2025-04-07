581fe44e4a3ca3cf49178689aac573f494831062
문인규 <ansdlsrb1121@naver.com>
2025-04-07 16:08:14 +0900

    [BE] Feature - Weekly Report 기능 추가

--- Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
+++ Back/music-note/main/src/main/java/com/music/note/main/controller/PreferencesController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.music.note.common.response.CommonResponse;
+import com.music.note.kafkaeventmodel.dto.WeeklyReportEvent;
+import com.music.note.main.kafka.producer.RequestEventProducer;
 import com.music.note.main.service.PreferencesService;
 import com.music.note.main.service.SpotifyService;
 
@@ -16,6 +18,7 @@ public class PreferencesController {
 
 	private final PreferencesService preferencesService;
 	private final SpotifyService spotifyService;
+	private final RequestEventProducer requestEventProducer;
 
 	@GetMapping("/preferences")
 	public CommonResponse<String> preferences(
@@ -26,5 +29,18 @@ public class PreferencesController {
 		preferencesService.publishUserMusicPreferences(Long.parseLong(userId), spotifyAccessToken);
 
 		return CommonResponse.success("음악 타입 결과 요청 성공");
+	}
+
+	// TODO : test 용도
+	@GetMapping("/weekly")
+	public CommonResponse<String> weeklyReport(@RequestHeader("X-User-Id") String userId) {
+		WeeklyReportEvent event = WeeklyReportEvent.builder()
+			.userId(Long.parseLong(userId))
+			.year(2025)
+			.month(4)
+			.day(5)
+			.build();
+		requestEventProducer.testWeeklyEvent(event);
+		return CommonResponse.success("주간 리포트 테스트 요청 성공");
 	}
 }
