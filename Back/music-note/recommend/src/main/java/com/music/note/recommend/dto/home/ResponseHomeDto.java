4c56c060deeef64004672084ecb25351dbc862c8
sonnbeom <dhsmf4245@gmail.com>
2025-04-08 13:17:35 +0900

    feat: read home data

---
+++ Back/music-note/recommend/src/main/java/com/music/note/recommend/dto/home/ResponseHomeDto.java
@@ -0,0 +1,23 @@
+package com.music.note.recommend.dto.home;
+
+import java.util.List;
+
+import com.music.note.recommend.dto.music.RecommendMusicDto;
+import com.music.note.recommend.dto.report.ResponseReportDto;
+import com.music.note.recommend.dto.report.music.MusicDto;
+import com.music.note.recommend.dto.type.TypeDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseHomeDto {
+	private TypeDto typeDto;
+	private String todayMessage;
+	private List<MusicDto> musicDtoList;
+}
