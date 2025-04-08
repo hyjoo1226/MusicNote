adc743fdabbe4fc6447c6d1af27b8bb7d8fc8e27
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-04-08 13:37:33 +0900

    feat: 추천 보관함 api 연결

--- Front/musicnote/src/pages/recommend/MyRecommendation.tsx
+++ Front/musicnote/src/pages/recommend/MyRecommendation.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import TopBar from "@/components/layout/TopBar";
-import recentPlayedList from "@/assets/data/recent-played-list.json";
 import Mascot from "@/assets/logo/mascot.webp";
 import { useGetData } from "@/hooks/useApi";
 
@@ -32,25 +31,22 @@ interface Book {
 
 export default function MyRecommendation() {
   const [movies, setMovies] = useState<Movie[]>([]);
-  const [music, setMusic] = useState<Music[]>([]);
-  const [book, setBook] = useState<Book[]>([]);
+  const [musics, setMusics] = useState<Music[]>([]);
+  const [books, setBooks] = useState<Book[]>([]);
 
   const navigate = useNavigate();
-  const {
-    data: likedMovies,
-    isLoading: likedMoviesLoading,
-    isError: likedMoviesError,
-  } = useGetData("likedMovies", "recommend/like/movie");
-  const {
-    data: likedMusic,
-    isLoading: likedMusicLoading,
-    isError: likedMusicError,
-  } = useGetData("likedMusic", "recommend/like/music");
-  const {
-    data: likedBook,
-    isLoading: likedBookLoading,
-    isError: likedBookError,
-  } = useGetData("likedBook", "recommend/like/book");
+  const { data: likedMovies, isError: likedMoviesError } = useGetData(
+    "likedMovies",
+    "recommend/like/movie"
+  );
+  const { data: likedMusic, isError: likedMusicError } = useGetData(
+    "likedMusic",
+    "recommend/like/music"
+  );
+  const { data: likedBook, isError: likedBookError } = useGetData(
+    "likedBook",
+    "recommend/like/book"
+  );
 
   useEffect(() => {
     if (likedMovies) {
@@ -61,14 +57,14 @@ export default function MyRecommendation() {
 
   useEffect(() => {
     if (likedMusic) {
-      setMusic(likedMusic.data.music);
+      setMusics(likedMusic.data.music);
     }
     console.log(likedMusic);
   }, [likedMusic]);
 
   useEffect(() => {
     if (likedBook) {
-      setBook(likedBook.data.books);
+      setBooks(likedBook.data.books);
     }
     console.log(likedBook);
   }, [likedBook]);
@@ -139,7 +135,7 @@ export default function MyRecommendation() {
       <div className="flex flex-col items-center justify-center w-auto mx-3 xs:mx-5 mb-3 xs:mb-5 p-2 bg-level2 rounded-sm">
         <div className="flex flex-row items-end justify-between w-full px-2">
           <h3 className="text-xl h-[26px] xs:h-[30px] xs:text-2xl font-bold">음악</h3>
-          {recentPlayedList.items.length > 0 && (
+          {musics.length > 0 && (
             <button
               className="text-light-gray text-sm xs:text-base cursor-pointer"
               onClick={() => navigate("/recommendations/my/music")}
@@ -148,26 +144,26 @@ export default function MyRecommendation() {
             </button>
           )}
         </div>
-        {recentPlayedList.items.length > 0 ? (
+        {musics.length > 0 ? (
           <div className="content-box grid grid-cols-3 sm:grid-cols-4 gap-x-2 gap-y-2 bg-level2 rounded-sm m-1 p-2 min-h-[290px] sm:min-h-[146px] max-h-[435px] sm:max-h-[440px] h-[calc((100vw-60px)/3*5/3*2)] xs:h-[calc((100vw-75px)/3*5/3*2)] sm:h-[calc((100vw-75px)/3*5/4*2)] min-w-[280px] max-w-[535px] w-[calc(100vw-45px)] xs:w-[calc(100vw-65px)]">
-            {recentPlayedList.items.map(
+            {musics.map(
               (music, index) =>
                 index < 8 && (
                   <div
-                    key={music.track.id}
+                    key={music.id}
                     className={`bg-level3 w-full aspect-[2/3] rounded-sm text-center ${index >= 6 ? "hidden sm:block" : ""}`}
                   >
                     <img
-                      src={music.track.album.images[0].url}
-                      alt={music.track.name}
+                      src={music.albumcover_path}
+                      alt={music.track_name}
                       className="w-full aspect-[1/1] object-cover rounded-t-sm mb-1 xs:mb-2"
                     />
                     <div className="flex flex-col items-center justify-center h-2/5">
                       <span className="flex items-center justify-center text-sm overflow-hidden text-ellipsis line-clamp-2 h-[70%]">
-                        {music.track.name}
+                        {music.track_name}
                       </span>
                       <span className="text-xs text-light-gray text-ellipsis line-clamp-1 h-[30%]">
-                        {music.track.artists[0].name}
+                        {music.artist_name}
                       </span>
                     </div>
                   </div>
@@ -203,18 +199,18 @@ export default function MyRecommendation() {
       <div className="flex flex-col items-center justify-center w-auto mx-3 xs:mx-5 mb-3 xs:mb-5 p-2 pb-0 bg-level2 rounded-md">
         <div className="flex flex-row items-end justify-between w-full px-2">
           <h3 className="text-xl h-[26px] xs:h-[30px] xs:text-2xl font-bold">책</h3>
-          {book.length > 0 && (
+          {books.length > 0 && (
             <span
               className="text-light-gray text-sm xs:text-base cursor-pointer"
-              onClick={() => navigate("/recommendations/my/movie")}
+              onClick={() => navigate("/recommendations/my/book")}
             >
               전체보기
             </span>
           )}
         </div>
-        {book.length > 0 ? (
+        {books.length > 0 ? (
           <div className="content-box grid grid-cols-3 sm:grid-cols-4 gap-x-2 gap-y-2 bg-level3 rounded-sm m-1 p-2 min-h-[290px] max-h-[435px] sm:min-h-[146px] h-[calc((100vw-60px)/3*5/3*2)] xs:h-[calc((100vw-75px)/3*5/3*2)] sm:h-[calc((100vw-75px)/3*5/2)] min-w-[280px] max-w-[535px] w-[calc(100vw-45px)] xs:w-[calc(100vw-65px)]">
-            {book.map(
+            {books.map(
               (book, index) =>
                 index < 8 && (
                   <div key={book.isbn} className={`${index >= 6 ? "hidden sm:block" : ""}`}>
@@ -239,7 +235,7 @@ export default function MyRecommendation() {
               </span>
               <button
                 className="bg-main w-auto text-white text-sm xs:text-base sm:text-sm md:text-base mx-2 px-2 py-1 rounded-xl"
-                onClick={() => navigate("/recommendations/detail/movie")}
+                onClick={() => navigate("/recommendations/detail/book")}
               >
                 책 추천
                 <br />
