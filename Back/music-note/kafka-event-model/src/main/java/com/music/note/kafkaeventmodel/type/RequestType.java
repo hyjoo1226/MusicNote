d2b5067e579560d9c67c02dd5fa7c06b82cbf4c0
InGyu-Moon <ansdlsrb1121@naver.com>
2025-04-10 19:37:58 +0900

    feat: add error notification using sse

--- Back/music-note/kafka-event-model/src/main/java/com/music/note/kafkaeventmodel/type/RequestType.java
+++ Back/music-note/kafka-event-model/src/main/java/com/music/note/kafkaeventmodel/type/RequestType.java
@@ -8,7 +8,12 @@ import lombok.RequiredArgsConstructor;
 public enum RequestType {
 	MANUAL("수동 요청"),
 	AUTOMATIC("자동 요청"),
-	WEEKLY("주간 요청");
+	WEEKLY("주간 요청"),
+	DAILY_ERROR("일일 리포트 생성에 실패했어요."),
+	WEEKLY_ERROR("주간 리포트 생성에 실패했어요."),
+	CRAWLING_ERROR("음악 데이터 크롤링에 실패했어요."),
+	DAILY_REPORTS_NOT_ENOUGH_FOR_WEEKLY("주간 리포트 생성을 위한 일간 리포트가 부족해요."),
+	;
 
 	private final String label;
 
