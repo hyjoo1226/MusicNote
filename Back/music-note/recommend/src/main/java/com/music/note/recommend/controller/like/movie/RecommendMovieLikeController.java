c74e130f68ba9be8649c59a2c6ed67835a69e169
손승범 <no1beom@naver.com>
2025-04-10 10:45:52 +0900

    [BE] Feat 추천 서버 logging 데이터 추가

--- Back/music-note/recommend/src/main/java/com/music/note/recommend/controller/like/movie/RecommendMovieLikeController.java
+++ Back/music-note/recommend/src/main/java/com/music/note/recommend/controller/like/movie/RecommendMovieLikeController.java
@@ -1,6 +1,8 @@
 package com.music.note.recommend.controller.like.movie;
 
 
+import static com.music.note.recommend.constant.log.like.LikeConstantAction.*;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.music.note.common.response.CommonResponse;
 import com.music.note.jwt.util.JwtUtil;
+import com.music.note.recommend.common.logging.dto.LogEvent;
+import com.music.note.recommend.common.logging.service.LoggingService;
 import com.music.note.recommend.dto.like.movie.request.RequestRecommendMovieLikeDto;
 import com.music.note.recommend.dto.movie.response.ResponseRecommendMovieList;
 import com.music.note.recommend.service.like.movie.RecommendMovieLikeService;
@@ -26,6 +30,7 @@ import lombok.extern.log4j.Log4j2;
 public class RecommendMovieLikeController {
 
 	private final RecommendMovieLikeService recommendMovieLikeService;
+	private final LoggingService loggingService;
 	@Value("${jwt.secret}")
 	private String secretKey;
 	@PostMapping("/like/movie")
@@ -34,6 +39,11 @@ public class RecommendMovieLikeController {
 		@RequestBody RequestRecommendMovieLikeDto requestRecommendMovieLikeDto) {
 		String userId = JwtUtil.getUserIdByJwtToken(request, secretKey);
 		recommendMovieLikeService.likeRecommendMovie(userId, requestRecommendMovieLikeDto);
+
+		// 추천 컨텐츠 좋아요한 유저의 행동 체크
+		loggingService.log(LogEvent.userAction(userId, LIKE_MOVIE_CREATE));
+
+
 		return CommonResponse.success("ok");
 	}
 	@GetMapping("/like/movie")
@@ -41,6 +51,10 @@ public class RecommendMovieLikeController {
 		HttpServletRequest request) {
 		String userId = JwtUtil.getUserIdByJwtToken(request, secretKey);
 		ResponseRecommendMovieList responseRecommendMovieList = recommendMovieLikeService.readLikeRecommendMovie(userId);
+
+		// 추천 컨텐츠 좋아요한 유저의 행동 체크
+		loggingService.log(LogEvent.userAction(userId, LIKE_MOVIE_READ));
+
 		return CommonResponse.success(responseRecommendMovieList);
 	}
 
@@ -50,6 +64,9 @@ public class RecommendMovieLikeController {
 		@RequestBody RequestRecommendMovieLikeDto requestRecommendMovieLikeDto) {
 		String userId = JwtUtil.getUserIdByJwtToken(request, secretKey);
 		recommendMovieLikeService.cancelRecommendMovieLike(userId, requestRecommendMovieLikeDto);
+
+		// 추천 컨텐츠 좋아요한 유저의 행동 체크
+		loggingService.log(LogEvent.userAction(userId, LIKE_MOVIE_DELETE));
 		return CommonResponse.success("ok");
 	}
 }
