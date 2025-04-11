ddce5cfc7f44c7ad827ea2625b4f942453ed0ce3
hyjoo1226 <sparrow9576@gmail.com>
2025-04-11 09:09:50 +0900

    fix: 책 추천 null 처리

--- Front/musicnote/src/features/recommend/BookCarousel.tsx
+++ Front/musicnote/src/features/recommend/BookCarousel.tsx
@@ -40,7 +40,9 @@ export default function BookCarousel({ books }: BookCarouselProps) {
                   {book.title}
                 </h2>
                 <div className="flex items-center gap-4 text-white/90 mb-2">
-                  <span className="text-xl text-light-gray">{book.pubdate.split("-")[0]}</span>
+                  <span className="text-xl text-light-gray">
+                    {(book.pubdate ? book.pubdate : "2024-08-27").split("-")[0]}
+                  </span>
                 </div>
                 <div className="flex flex-wrap gap-2">
                   {book.author.split(",").map((author) => (
